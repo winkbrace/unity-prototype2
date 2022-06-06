59a3f4d171807b6f707b0bf8927b0939a614b876
Bas de Ruiter <bas@basbv.it>
2022-06-06 17:18:58 +0200

    Add challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastFiredAt;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var now = Time.time;
+            
+            if (now - lastFiredAt < 1.0f) {
+                Debug.Log("Recently fired a dog");
+                return;
+            }
+            
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastFiredAt = now;
+        }
+    }
+}
